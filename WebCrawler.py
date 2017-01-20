d0c33b171c05d0fb6a8c6cbedc126078f103cc95
Manu <gmanu21097@gmail.com>
2017-01-20 23:55:01 +0530

    Crawler #2

--- WebCrawler.py
+++ WebCrawler.py
@@ -2,4 +2,8 @@ import requests
 from bs4 import beautifulsoup
 
 def webcrawler(max_pages):
-    
+    page=1
+    while page <= max_pages:
+        url='https://thenewboston.com/forum/recent_activity.php?page='+str(page)
+        source_code=requests.get(url)
+        
