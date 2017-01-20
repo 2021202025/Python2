50720b8c2dde78cce3d8e06e53738b57d329b851
Manu <gmanu21097@gmail.com>
2017-01-21 00:13:22 +0530

    Crawler#3

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,9 +1,15 @@
 import requests
-from bs4 import beautifulsoup
+from bs4 import BeautifulSoup
 
 def webcrawler(max_pages):
     page=1
     while page <= max_pages:
-        url='https://thenewboston.com/forum/recent_activity.php?page='+str(page)
-        source_code=requests.get(url)
-        
+        url='https://thenewboston.com/forum/recent_activity.php?page=' +str(page)
+        source_code = requests.get(url)
+        text = source_code.txt
+        Soup = BeautifulSoup(text)
+        for link in Soup.findall('a', {'class': 'title text-semibold'}):
+            href = "https://thenewboston.com" + link.get('href')
+            print(href)
+
+webcrawler(1)
