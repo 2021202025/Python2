0904a064d1dd98b5efc528829476ec300099ecfe
Manu <gmanu21097@gmail.com>
2017-01-22 21:37:01 +0530

    Other Changes

--- WebCrawler.py
+++ WebCrawler.py
@@ -6,4 +6,8 @@ url = "https://twitter.com/FordMustang"
 this_page = urllib.request.urlopen(url)
 soup = BeautifulSoup(this_page, "html.parser")
 
-print(soup.title)
+print(soup.title.text)
+
+for link in (soup.findAll('a')):
+    print(link.get('href'))
+    print(link.text)
