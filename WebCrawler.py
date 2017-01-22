6961d0cf88677601e4f49ad2b5a9c80318b9de70
Manu <gmanu21097@gmail.com>
2017-01-22 21:00:43 +0530

    Changes

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,3 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+yellow = requests.get("http://www.yellowpages.com/search?search_terms=Coffee&geo_location_terms=Los+Angeles%2C+CA")
+
+text = yellow.txt
+Soup = BeautifulSoup(text)
+Soup.findAll("a")
