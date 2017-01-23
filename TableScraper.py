fb3ddfa702cf6320bcce8281bda1cf96108165e1
Manu <gmanu21097@gmail.com>
2017-01-23 23:31:43 +0530

    Final Scraper

--- TableScraper.py
+++ TableScraper.py
@@ -1,6 +1,7 @@
 import urllib
 import urllib.request
 from bs4 import BeautifulSoup
+import os
 
 def make_soup(url):
     page = urllib.request.urlopen(url)
@@ -9,8 +10,15 @@ def make_soup(url):
 
 soup = make_soup("http://www.basketball-reference.com/players/a/")
 
-playerdata = " "
+playerdatasaved = " "
 for record in soup.findAll('tr'):
+    playerdata = ""
     for data in record.findAll('td'):
         playerdata = playerdata+","+data.text
-    print(playerdata)
+    playerdatasaved = playerdatasaved + "\n" + playerdata[1:]
+
+header = "Player,From,To,Pos,Ht,Wt,Birth Date,College" + "\n"
+file = open(os.path.expanduser("Basketball.csv"),"wb")
+file.write(bytes(header, encoding="ascii",errors='ignore'))
+file.write(bytes(playerdatasaved, encoding="ascii",errors='ignore'))
+
---
+++ shadow.py
@@ -0,0 +1,8 @@
+num_array = list()
+num = input("Enter how many elements you want:")
+print ('Enter numbers in array: ')
+for i in range(int(num)):
+    n = input("num :")
+    num_array.append(int(n))
+for i in range(int(num)):
+    print (num_array[i])
