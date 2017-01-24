aa42dee90be06fa728ed9e849cdb7867c242582f
Manu <gmanu21097@gmail.com>
2017-01-24 13:57:14 +0530

    Multiple Pages

--- TableScraper.py
+++ TableScraper.py
@@ -2,15 +2,16 @@ import urllib
 import urllib.request
 from bs4 import BeautifulSoup
 import os
+from string import ascii_lowercase
 
 def make_soup(url):
     page = urllib.request.urlopen(url)
     soupdata = BeautifulSoup(page, "html.parser")
     return soupdata
 
-soup = make_soup("http://www.basketball-reference.com/players/a/")
-
 playerdatasaved = " "
+for letter in ascii_lowercase:
+    soup = make_soup("http://www.basketball-reference.com/players/" + letter + "/")
 for record in soup.findAll('tr'):
     playerdata = ""
     for data in record.findAll('td'):
@@ -18,7 +19,7 @@ for record in soup.findAll('tr'):
     playerdatasaved = playerdatasaved + "\n" + playerdata[1:]
 
 header = "Player,From,To,Pos,Ht,Wt,Birth Date,College" + "\n"
-file = open(os.path.expanduser("Basketball.csv"),"wb")
+file = open(os.path.expanduser("Basketballtable.csv"),"wb")
 file.write(bytes(header, encoding="ascii",errors='ignore'))
 file.write(bytes(playerdatasaved, encoding="ascii",errors='ignore'))
 
